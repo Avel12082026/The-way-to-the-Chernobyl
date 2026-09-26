0593fef986b0eb66ad40b4f8b49712bc8dc52f2e
Avel12082026 <avelsvobodniy@gmail.com>
2026-09-27 01:19:54 +0900

    test: pin new Rostok map checksum

--- tests/rostok_location_installer.test.py
+++ tests/rostok_location_installer.test.py
@@ -8,7 +8,7 @@ spec=importlib.util.spec_from_file_location('rostok_installer',path)
 mod=importlib.util.module_from_spec(spec);spec.loader.exec_module(mod)
 
 assert mod.CACHE_KEY=='20260926-agroprom-map-hq1'
-assert mod.MAP_SHA=='017f3b41e187a44f33505bd007374ae3a2281c2cefc7bdda1c1ab0bc69ac22aa'
+assert mod.MAP_SHA=='faf49a24a7b3a9965e137e8251536639d5adb738673d1d9b14295c733cc11cee'
 assert mod.BAR_SHA=='bf138d0c05afc2c4d65c504a135d35a1b3af3ecf74ebe8e740d7c3be5b17054c'
 assert mod.MAP_SIZE==(865,1536)
 assert mod.BAR_SIZE==(941,1672)
